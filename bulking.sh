7f677f5480ce11ed1c4d1b2621a0d115a5a645bf
kiran.lakhani <kiran.lakhani@servicenow.com>
2022-03-02 11:05:58 +0530

    10001:randfile_10001

--- bulking.sh
+++ bulking.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
-j=9000
-while [ $x -le 208 ]
+j=10000
+while [ $x -le 5 ]
 do
  string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
